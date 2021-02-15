fc09fa05bfd30b9450098f12d78fff81bfa40a54
Andrew Borges <andrewborges71@gmail.com>
2021-02-15 12:07:15 -0300

    Exercicio sobre interfaces

---
+++ Interfaces/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using Interfaces.Entities;
+using Interfaces.Services;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter contract data");
+
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Date (dd/MM/yyyy): ");
+            DateTime date = DateTime.ParseExact(Console.ReadLine(), "dd/MM/yyyy", CultureInfo.InvariantCulture);
+            Console.Write("Contract value: ");
+            double value = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Contract contract = new Contract(number, date, value);
+
+            Console.Write("Enter number of installments: ");
+            int months = int.Parse(Console.ReadLine());
+
+            ContractService service = new ContractService(new PaypalService());
+            service.ProcessContract(contract, months);
+
+            Console.WriteLine("Installments:");
+            foreach(Installment ins in contract.Installments)
+            {
+                Console.WriteLine(ins);
+            }
+        }
+    }
+}
